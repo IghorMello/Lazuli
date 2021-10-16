5bd06c7fbebf859a7cb3d226cf9a23649e90d67e
SAVIO AUGUSTO LOPES <savio.lopes@fatec.sp.gov.br>
2021-10-16 13:58:18 -0300

    :memo: Refactor app and fix bug

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ import logging
 from config import config_dict
 from app import create_app, db
 
-# WARNING: Don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
-
-# The configuration
 get_config_mode = 'Debug' if DEBUG else 'Production'
 
 try:
@@ -25,9 +22,9 @@ app = create_app( app_config )
 Migrate(app, db)
 
 if DEBUG:
-    app.logger.info('DEBUG       = ' + str(DEBUG)      )
+    app.logger.info('DEBUG = ' + str(DEBUG)      )
     app.logger.info('Environment = ' + get_config_mode )
-    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI )
+    app.logger.info('DBMS = ' + app_config.SQLALCHEMY_DATABASE_URI )
 
 if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -4,38 +4,30 @@ import os
 from decouple import config
 
 class Config(object):
-    basedir    = os.path.abspath(os.path.dirname(__file__))
-
-    # Set up the App SECRET_KEY
+    basedir = os.path.abspath(os.path.dirname(__file__))
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
-
-    # This will create a file in <app> FOLDER
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
     DEBUG = False
-
-    # Security
-    SESSION_COOKIE_HTTPONLY  = True
+    SESSION_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_DURATION = 3600
 
-    # PostgreSQL database
     SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
-        config( 'DB_ENGINE'   , default='postgresql'    ),
-        config( 'DB_USERNAME' , default='appseed'       ),
-        config( 'DB_PASS'     , default='pass'          ),
-        config( 'DB_HOST'     , default='localhost'     ),
-        config( 'DB_PORT'     , default=5432            ),
-        config( 'DB_NAME'     , default='appseed-flask' )
+        config('DB_ENGINE', default='postgresql'),
+        config('DB_USERNAME', default='teste'),
+        config('DB_PASS', default='pass'),
+        config('DB_HOST', default='localhost'),
+        config('DB_PORT', default=5432),
+        config('DB_NAME', default='teste-flask')
     )
 
 class DebugConfig(Config):
     DEBUG = True
 
-# Load all possible configurations
 config_dict = {
     'Production': ProductionConfig,
-    'Debug'     : DebugConfig
+    'Debug': DebugConfig
 }
