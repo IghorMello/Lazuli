3b5c36e8960fe55e6b58d8b6577210a9bb688227
SAVIO AUGUSTO LOPES <savio.lopes@fatec.sp.gov.br>
2021-10-16 14:13:21 -0300

    :memo: Refactor app and fix bug

--- app/base/forms.py
+++ app/base/forms.py
@@ -1,19 +1,16 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
 
 from flask_wtf import FlaskForm
 from wtforms import TextField, PasswordField
 from wtforms.validators import InputRequired, Email, DataRequired
 
-## login and registration
+## Login e cadastro
 
 class LoginForm(FlaskForm):
-    username = TextField    ('Username', id='username_login'   , validators=[DataRequired()])
-    password = PasswordField('Password', id='pwd_login'        , validators=[DataRequired()])
+    username = TextField('Nome', id='username_login', validators=[DataRequired()])
+    password = PasswordField('CRM', id='pwd_login', validators=[DataRequired()])
 
 class CreateAccountForm(FlaskForm):
-    username = TextField('Username'     , id='username_create' , validators=[DataRequired()])
-    email    = TextField('Email'        , id='email_create'    , validators=[DataRequired(), Email()])
-    password = PasswordField('Password' , id='pwd_create'      , validators=[DataRequired()])
+    username = TextField('Usuário', id='username_create', validators=[DataRequired()])
+    email= TextField('Email organizacional', id='email_create', validators=[DataRequired(), Email()])
+    password = PasswordField('CRM', id='pwd_create', validators=[DataRequired()])
--- app/base/models.py
+++ app/base/models.py
@@ -1,17 +1,11 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
 
 from flask_login import UserMixin
 from sqlalchemy import Binary, Column, Integer, String
-
 from app import db, login_manager
-
 from app.base.util import hash_pass
 
 class User(db.Model, UserMixin):
-
     __tablename__ = 'User'
 
     id = Column(Integer, primary_key=True)
@@ -21,21 +15,15 @@ class User(db.Model, UserMixin):
 
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
-            # depending on whether value is an iterable or not, we must
-            # unpack it's value (when **kwargs is request.form, some values
-            # will be a 1-element list)
             if hasattr(value, '__iter__') and not isinstance(value, str):
-                # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
                 value = value[0]
 
             if property == 'password':
-                value = hash_pass( value ) # we need bytes here (not plain str)
-                
+                value = hash_pass( value )
             setattr(self, property, value)
 
     def __repr__(self):
         return str(self.username)
-
 
 @login_manager.user_loader
 def user_loader(id):
