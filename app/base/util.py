2f4d3339e54b3d41c125d27d5f4eb02c7b256e78
SAVIO AUGUSTO LOPES <savio.lopes@fatec.sp.gov.br>
2021-10-16 15:41:05 -0300

    :memo: Refactor app and fix bug

--- app/base/util.py
+++ app/base/util.py
@@ -2,11 +2,14 @@
 
 import hashlib, binascii, os
 
-def hash_pass( password ):
+def hash_pass(password):
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
     pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)
     return (salt + pwdhash) 
+
+
+
 
 def verify_pass(provided_password, stored_password):
     stored_password = stored_password.decode('ascii')
