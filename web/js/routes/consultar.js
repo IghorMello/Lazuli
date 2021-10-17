6ae97b54aae1d41a0282701055ab94535b901899
savio-2-lopes <savioaugulopes@gmail.com>
2021-10-17 16:09:50 -0300

    :tada: Refactor api and fix bug

--- web/js/routes/consultar.js
+++ web/js/routes/consultar.js
@@ -8,11 +8,11 @@ $.ajax({
   method: 'get',
   success: function (data) {
     var html = '';
-    var count = data
-    var total = Object.keys(count).length;
+    counter = 0
     for (var i in data) {
+      counter += 1
       console.log(data[i]['_id']['$oid'])
-      html += "<tr><th>'" + data[i]['_id']['$oid'] + "'</th><th>'" + data[i]['nome'] + "'</th><th>'" + data[i]['email'] + "'</th><th>'" + data[i]['codigo_usuario'] + "'</th></tr>"
+      html += "<tr><th>'" + counter + "'</th><th>'" + data[i]['nome'] + "'</th><th>'" + data[i]['email'] + "'</th><th>'" + data[i]['codigo_usuario'] + "'</th></tr>"
     }
     $('#exibir').html(html)
   },
