9c4784b7ec01014186de23b7c172d6efa41cd6fd
savio-2-lopes <savioaugulopes@gmail.com>
2021-11-02 12:49:28 -0300

    :tada: Refactor api and fix bug

--- web/app.py
+++ web/app.py
@@ -109,5 +109,5 @@ def sign_out():
     # session.pop('username')
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-  app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
