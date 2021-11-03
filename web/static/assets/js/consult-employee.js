7046401527b9b152a2b36e05588f2435c61badd0
savio-2-lopes <savioaugulopes@gmail.com>
2021-11-03 12:02:42 -0300

    :tada: Refactor api and fix bug

--- web/static/assets/js/consult-employee.js
+++ web/static/assets/js/consult-employee.js
@@ -10,7 +10,7 @@ $.ajax({
     var html = '';
     counter = 0
     for (var i in data) {
-      html += "<tr><td><input class='sr-only' id='action' value='" + data[i]['_id']['$oid'] + "'><a href='#' class='fw-bold'>'" + data[i]['codigo_usuario'] + "'</a><td><span class='fw-normal'>'" + data[i]['email'] + "'</span></td><td><span class='fw-normal'>'" + data[i]['nome'] + "'</td><td><span class='fw-bold text-warning'>'" + data[i]['status'] + "'</td><td><a class='dropdown-item'  value='" + data[i]['_id']['$oid'] + "' href='#'><span class='fa fa-pencil-square-o me-2'></span>Editar</a><a class='dropdown-item text-danger rounded-bottom' value='" + data[i]['_id']['$oid'] + "' onclick='excluir()' href='#'><span class='fa fa-trash me-2'></span>Remover</a></td></tr>"
+      html += "<tr onclick='visualizar()'><td><input class='sr-only' id='action' value='" + data[i]['_id']['$oid'] + "'><a href='#' class='fw-bold'>'" + data[i]['codigo_usuario'] + "'</a><td><span class='fw-normal'>'" + data[i]['email'] + "'</span></td><td><span class='fw-normal'>'" + data[i]['nome'] + "'</td><td><span class='fw-bold text-warning'>'" + data[i]['status'] + "'</td><td><a class='dropdown-item'  value='" + data[i]['_id']['$oid'] + "' href='#'><span onclick='editar()'class='fa fa-pencil-square-o me-2'></span>Editar</a><a class='dropdown-item text-danger rounded-bottom' value='" + data[i]['_id']['$oid'] + "' onclick='excluir()' href='#'><span class='fa fa-trash me-2'></span>Remover</a></td></tr>"
     }
     $('#exibir').html(html)
   },
@@ -21,11 +21,16 @@ $.ajax({
 });
 
 function excluir() {
-  var localid = $('#excluir').val();
+  var localid = $('#action').val();
   console.log(localid)
 }
 
 function editar() {
+  var localid = $('#action').val();
+  console.log(localid)
+}
+
+function visualizar() {
   var localid = $('#action').val();
   console.log(localid)
 }
