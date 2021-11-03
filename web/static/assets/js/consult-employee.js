fc5eb5b0c63f4f926361fb406c1b075f6373c945
savio-2-lopes <savioaugulopes@gmail.com>
2021-11-03 10:39:43 -0300

    :tada: Refactor api and fix bug

--- web/static/assets/js/consult-employee.js
+++ web/static/assets/js/consult-employee.js
@@ -10,9 +10,7 @@ $.ajax({
     var html = '';
     counter = 0
     for (var i in data) {
-      counter += 1
-      console.log(data[i]['_id']['$oid'])
-      html += "<tr><th>'" + counter + "'</th><th>'" + data[i]['nome'] + "'</th><th>'" + data[i]['email'] + "'</th><th>'" + data[i]['codigo_usuario'] + "'</th></tr>"
+      html += "<tr><td><a href='#' class='fw-bold'>'" + data[i]['codigo_usuario'] + "'</a><td><span class='fw-normal'>'" + data[i]['email'] + "'</span></td><td><span class='fw-normal'>'" + data[i]['nome'] + "'</td><td><span class='fw-bold text-warning'>'" + data[i]['status'] + "'</td><td><a class='dropdown-item'  value='" + data[i]['_id']['$oid'] + "' href='#'><span class='fa fa-pencil-square-o me-2'></span>Editar</a><a class='dropdown-item text-danger rounded-bottom' value='" + data[i]['_id']['$oid'] + "' href='#'><span class='fa fa-trash me-2'></span>Remover</a></td></tr>"
     }
     $('#exibir').html(html)
   },
