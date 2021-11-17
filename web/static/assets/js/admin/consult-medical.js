8c745a76c271699f7654db9c624d5fe67755a907
savio-2-lopes <savioaugulopes@gmail.com>
2021-11-17 15:20:45 -0300

    Refactor app and fix bug

--- web/static/assets/js/admin/consult-medical.js
+++ web/static/assets/js/admin/consult-medical.js
@@ -3,7 +3,7 @@
 //--------------------
 
 $.ajax({
-  url: "https://flaskapideploy.herokuapp.com/employees",
+  url: "https://flaskapideploy.herokuapp.com/medical/list",
   method: "get",
   success: function (data) {
     var html = "";
@@ -14,28 +14,16 @@ $.ajax({
         "<tr><td><input class='sr-only' id='action' value='" +
         data[i]["_id"]["$oid"] +
         "'><a href='#' class='fw-bold'>'" +
-        data[i]["codigo_usuario"] +
+        data[i]["crm"] +
         "'</a><td><span class='fw-normal'>'" +
         data[i]["email"] +
         "'</span></td><td><span class='fw-normal'>'" +
         data[i]["nome"] +
         "'</td><td><span class='fw-bold text-warning'>'" +
         data[i]["status"] +
-        "'</td><td><a class='dropdown-item' value='" +
-        data[i]["_id"]["$oid"] +
-        "' href='/admin/edit/" +
-        data[i]["_id"]["$oid"] +
-        "'><span onclick='editar()' class='fa fa-pencil-square-o me-2'></span>Editar</a><a class='dropdown-item text-danger rounded-bottom' value='" +
-        data[i]["_id"]["$oid"] +
-        "' onclick='excluir()' href='#'><span class='fa fa-trash me-2'></span>Remover</a><a class='dropdown-item text-info rounded-bottom' value='" +
-        data[i]["_id"]["$oid"] +
-        "' onclick='visualizar()' href='/admin/consult/" +
-        data[i]["_id"]["$oid"] +
-        "'><span class='fa fa-eye me-2'></span>Visualizar</a></td></tr>";
+        "'</td><td><a class='dropdown-item' href='#'><span onclick='editar()' class='fa fa-pencil-square-o me-2'></span>Editar</a><a class='dropdown-item text-danger rounded-bottom' onclick='excluir()' href='#'><span class='fa fa-trash me-2'></span>Remover</a><a class='dropdown-item text-info rounded-bottom'  onclick='visualizar()' href='#'><span class='fa fa-eye me-2'></span>Visualizar</a></td></tr>";
     }
-    console.log(counter);
     $("#exibir").html(html);
-    $("#number").html(counter);
   },
   error: function (error) {
     console.error(error);
@@ -47,29 +35,3 @@ $.ajax({
     });
   },
 });
-
-function excluir() {
-  var localid = $("#action").val();
-  console.log(localid);
-  $.ajax({
-    url: "https://flaskapideploy.herokuapp.com/employees/" + localid,
-    method: "delete",
-    success: function (data) {
-      console.log(data);
-      Swal.fire({
-        icon: "success",
-        title: "Deletado com sucesso!",
-      });
-      document.location.reload(true);
-    },
-    error: function (error) {
-      console.error(error);
-      Swal.fire({
-        icon: "warning",
-        text: "" + error,
-        showConfirmButton: false,
-        timer: 1500,
-      });
-    },
-  });
-}
