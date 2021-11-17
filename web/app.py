f6348e2a5c0facab6f7463f2653c2fde659af8a6
savio-2-lopes <savioaugulopes@gmail.com>
2021-11-17 13:53:34 -0300

    Refactor app and fix bug

--- web/app.py
+++ web/app.py
@@ -188,7 +188,7 @@ def medical_register_employee():
 @login_required
 def medical_consult_employee():
     current=session['current_user']
-    type_user=current_user['type_user']
+    type_user=current['type_user']
     if type_user != 'responsavel_medico':
       return redirect(url_for('login'))
     else:
